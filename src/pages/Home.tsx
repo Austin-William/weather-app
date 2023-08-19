74a3e77f6e670018cd3a6d4135e5a3ae357fe560
Austin-William Lo <austin-william.lo@epitech.eu>
2023-08-19 19:47:44 +0200

    [UPD] fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,22 @@ import Footer from "../components/Footer";
 import "../styles/pages/Home.scss";
 
 function Home() {
+    const [search, setSearch] = React.useState("");
+
+    const key = process.env.API_KEY;
+
+    function onSearch(value: string) {
+        if (value) {
+            setSearch(value);
+            console.log(value);
+            console.log(key);
+        }
+    }
+
     return (
         <div className="Home">
             <div className="container">
-                <Navbar />
+                <Navbar onSearch={onSearch} />
                 <Weather />
                 <Footer />
             </div>
